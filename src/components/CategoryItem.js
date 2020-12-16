ab10547ab3ba5f3a346416a1004638a2d090e606
f.mirzakhanloo <f.mirzakhanloo@dpr.co.com>
2020-12-17 01:56:33 +0330

    add svg assets

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {StyleSheet, Text, View, TouchableWithoutFeedback} from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 import PatternBackground from '../components/PatternBackground';
-import {logoWhite} from '../assets/svgs';
-// import {SvgXml} from 'react-native-svg';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { logoWhite } from '../assets/svgs';
+import { SvgXml } from 'react-native-svg';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Card from './Card';
-
-const CategoryItem = ({item, index, onPress}) => {
+const color = '#62BA7B'
+const CategoryItem = ({ item, index, onPress }) => {
     return (
         <TouchableWithoutFeedback onPress={onPress}>
             <Card style={styles.container}>
-                <Text style={styles.title}>{item.name}</Text>
-                <Text style={styles.row}>{index + 1}</Text>
-                {/*<SvgXml style={styles.logoStyle} width={50} height={50} xml={logoWhite}/>*/}
+                <View>
+                    <Text style={styles.title}>{item.name}</Text>
+                    <Text style={styles.row}>{index + 1}</Text>
+                </View>
+                <SvgXml style={styles.logoStyle} fill={color} width={50} height={50} xml={item.source} />
             </Card>
         </TouchableWithoutFeedback>
     );
--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -1,94 +1,93 @@
 import React from 'react';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import PatternBackground from '../components/PatternBackground';
-import {SvgXml} from 'react-native-svg';
-import {logoWhite} from '../assets/svgs';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { SvgXml } from 'react-native-svg';
+import { bipolar,schizophrenia,depression,stress} from '../assets/svgs';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import CategoryItem from '../components/CategoryItem';
 
 const CategoriesScreen = (props) => {
     const tempList = [
+         {
+             id:1,
+            source: bipolar,
+            name: 'تابلو های بالینی عصبی_رشدی',
+        },
         {
-            _id: '5fc4eca3b31a2d2dedf34dc9',
-            picture: 'http://placehold.it/32x32',
-            size: 1,
-            color: 'green',
-            name: 'MEDESIGN',
+            id:2,
+            source: schizophrenia,
+            name: 'اسکیزو فرنی و دیگر تابلو های روان پریشی',
+        },
+        {
+            id:3,
+            source: bipolar,
+            name: 'تابلو های بالینی دو قطبی',
+
+        },
+        {
+            id:4,
+            source: depression,
+            name: 'تابلو های بالینی افسردگی',
+        },
+        {
+            id:5,
+            source: stress,
+            name: 'تابلو های بالینی اضطراب',
+        },
+        {
+            id:6,
+            source: bipolar,
+            name: 'تابلوهای بالینی مربوط به تروما و عوامل استرس زا',
+        },
+        {
+            id:7,
+            source: bipolar,
+            name: 'تابلو های بالینی نشانه های جسمی',
+        },
+        {
+            id:8,
+            source: bipolar,
+            name: 'تابلو های بالینی خوردن',
+        },
+        {
+            id:9,
+            source: bipolar,
+            name: 'تابلو های بالینی خواب و بیداری',
         }, {
-            _id: '5fc4eca3b31a2d2dedf34dc9',
-            picture: 'http://placehold.it/32x32',
-            size: 1,
-            color: 'orange',
-            name: 'MEDESIGN',
-        },
-        {
-            _id: '5fc4eca3a0f784d04071b8e4',
-            picture: 'http://placehold.it/32x32',
-            size: 2,
-            color: 'blue',
-            name: 'ORBIFLEX',
-        },
-        {
-            _id: '5fc4eca3fe0cf09ab9d4020a',
-            picture: 'http://placehold.it/32x32',
-            size: 3,
-            color: 'gray',
-            name: 'ZENSUS',
-        },
-        {
-            _id: '5fc4eca32d7a7d53b722f6b7',
-            picture: 'http://placehold.it/32x32',
-            size: 3,
-            color: 'brown',
-            name: 'MINGA',
-        },
-        {
-            _id: '5fc4eca35cc8d2569e99175b',
-            picture: 'http://placehold.it/32x32',
-            size: 2,
-            color: 'pink',
-            name: 'FUELWORKS',
-        },
-        {
-            _id: '5fc4eca30759b12d30dbb790',
-            picture: 'http://placehold.it/32x32',
-            size: 2,
-            color: 'black',
-            name: 'ZENSOR',
-        }, {
-            _id: '5fc4eca30759b12d30dbb790',
-            picture: 'http://placehold.it/32x32',
-            size: 3,
-            color: 'red',
-            name: 'ZENSOR',
-        },
-        {
-            _id: '5fc4eca30759b12d30dbb790',
-            picture: 'http://placehold.it/32x32',
-            size: 3,
-            color: 'orange',
-            name: 'ZENSOR',
-        }, {
-            _id: '5fc4eca30759b12d30dbb790',
-            picture: 'http://placehold.it/32x32',
-            size: 2,
-            color: 'yellow',
-            name: 'ZENSOR',
+            id:10,
+            source: bipolar,
+            name: 'تابلو های بالینی کژکاری جنسی',
+        },{
+            id:11,
+            source: bipolar,
+            name: 'تابلو های بالینی اختلالات ایذایی؛کنترل تکانه و سلوک',
+        },{
+            id:12,
+            source: bipolar,
+            name: 'تابلوهای بالینی مربوط به مواد',
+        },{
+            id:13,
+            source: bipolar,
+            name: 'تابلوهای عصبی_شناختی',
+        },{
+            id:14,
+            source: bipolar,
+            name: 'تابلوهایی با سبب شناسی طبی',
         },
     ];
     return (
         <PatternBackground>
             <View style={styles.container}>
-                <SvgXml style={styles.logoStyle} width={100} height={100} xml={logoWhite}/>
+                <SvgXml style={styles.logoStyle} width={100} height={100} xml={bipolar} />
                 <Text style={styles.title}>شاخصه های اصلی</Text>
             </View>
             <FlatList
                 data={tempList}
-                style={{paddingHorizontal: wp('2%')}}
+                style={{ paddingHorizontal: wp('2%') }}
                 emptyListComponent={<View><Text>هنوز حسابی ایجاد نکرده اید</Text></View>}
                 keyExtractor={(item, index) => index.toString()}
-                renderItem={({item, index}) => (
-                    <CategoryItem item={item} index={index} onPress={() => props.navigation?.navigate('Forms')}/>
+                renderItem={({ item, index }) => (
+                    <CategoryItem item={item} index={index} onPress={() => props.navigation?.navigate('Forms')} />
                 )}
             />
         </PatternBackground>
