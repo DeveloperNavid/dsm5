5426418af199b9f99d68c96e56f0b0208f0f7f2e
f.mirzakhanloo <f.mirzakhanloo@dpr.co.com>
2020-12-18 12:11:31 +0330

    update svg

--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -2,78 +2,79 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import PatternBackground from '../components/PatternBackground';
 import { SvgXml } from 'react-native-svg';
-import { bipolar,schizophrenia,depression,stress,
-romantic,anxiety,physical,eating,insomnia,solok,drug,
-psychologist,asabiShenakhti,
+import {
+    bipolar, schizophrenia, depression, stress,
+    romantic, anxiety, physical, eating, insomnia, solok, drug,
+    psychologist, asabiShenakhti, asabiRoshdi
 } from '../assets/svgs';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import CategoryItem from '../components/CategoryItem';
 
 const CategoriesScreen = (props) => {
     const tempList = [
-         {
-             id:1,
-            source: bipolar,
+        {
+            id: 1,
+            source: asabiRoshdi,
             name: 'تابلو های بالینی عصبی_رشدی',
         },
         {
-            id:2,
+            id: 2,
             source: schizophrenia,
             name: 'اسکیزو فرنی و دیگر تابلو های روان پریشی',
         },
         {
-            id:3,
+            id: 3,
             source: bipolar,
             name: 'تابلو های بالینی دو قطبی',
 
         },
         {
-            id:4,
+            id: 4,
             source: depression,
             name: 'تابلو های بالینی افسردگی',
         },
         {
-            id:5,
+            id: 5,
             source: anxiety,
             name: 'تابلو های بالینی اضطراب',
         },
         {
-            id:6,
+            id: 6,
             source: stress,
             name: 'تابلوهای بالینی مربوط به تروما و عوامل استرس زا',
         },
         {
-            id:7,
+            id: 7,
             source: physical,
             name: 'تابلو های بالینی نشانه های جسمی',
         },
         {
-            id:8,
+            id: 8,
             source: eating,
             name: 'تابلو های بالینی خوردن',
         },
         {
-            id:9,
+            id: 9,
             source: insomnia,
             name: 'تابلو های بالینی خواب و بیداری',
         }, {
-            id:10,
+            id: 10,
             source: romantic,
             name: 'تابلو های بالینی کژکاری جنسی',
-        },{
-            id:11,
+        }, {
+            id: 11,
             source: solok,
             name: 'تابلو های بالینی اختلالات ایذایی؛کنترل تکانه و سلوک',
-        },{
-            id:12,
+        }, {
+            id: 12,
             source: drug,
             name: 'تابلوهای بالینی مربوط به مواد',
-        },{
-            id:13,
+        }, {
+            id: 13,
             source: asabiShenakhti,
             name: 'تابلوهای عصبی_شناختی',
-        },{
-            id:14,
+        }, {
+            id: 14,
             source: psychologist,
             name: 'تابلوهایی با سبب شناسی طبی',
         },
