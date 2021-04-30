be25d0738bdfaa991288ed7c0012b403e022b5cd
Farzaneh Mirzakhanloo <farzanm14@gmail.com>
2021-04-30 22:37:28 +0430

    refactor code

--- metro.config.js
+++ metro.config.js
@@ -9,6 +9,7 @@ module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
+
         experimentalImportSupport: false,
         inlineRequires: false,
       },
